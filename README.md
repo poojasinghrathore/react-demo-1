99772653aa94b28676dcc634dcfe4bef7b8bba06
poojasinghrathore <poojasingh9498@gmail.com>
2021-01-24 20:12:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mfqclw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mfqclw)
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  // function OnLight() {
+  //   test.src =
+  //     "https://cdn.mos.cms.futurecdn.net/8URLqwFeJihbNpn3gByhg3-1200-80.jpg";
+  // }
+  // function OffLight() {
+  //   test.src =
+  //     "https://upload.wikimedia.org/wikipedia/commons/3/3a/Gluehlampe_01_KMJ.jpg";
+  // }
+
+  const [state, setState] = React.useState(false);
+
+  const handleClick = event => {
+    setState(!state);
+  };
+
+  return (
+    <div>
+      <p>
+        <img
+          src={
+            state
+              ? "https://upload.wikimedia.org/wikipedia/commons/3/3a/Gluehlampe_01_KMJ.jpg"
+              : "https://cdn.mos.cms.futurecdn.net/8URLqwFeJihbNpn3gByhg3-1200-80.jpg"
+          }
+          style={{ width: "100px" }}
+        />
+      </p>
+      <button onClick={handleClick}>Turn {state ? `Off` : `On`} Light</button>
+    </div>
+  );
+}
